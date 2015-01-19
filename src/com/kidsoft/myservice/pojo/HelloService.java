cb45d6380df6eef4b770cfc15e9aacb27410104c
lion2016 <1033871755@qq.com>
2015-01-19 09:04:33 +0800

    webservice 项目创建

--- src/com/kidsoft/myservice/pojo/HelloService.java
+++
@@ -1,7 +0,0 @@
-package com.kidsoft.myservice.pojo;
-
-public class HelloService {
-	public String sayHello(String name){
-		return name+",你好!";
-	}
-}
---
+++ src/com/kidsoft/myweb/pojo/HelloService.java
@@ -0,0 +1,8 @@
+package com.kidsoft.myweb.pojo;
+
+public class HelloService {
+	public String sayHello(String yourName) {
+		// TODO Auto-generated method stub
+		return "你好,"+yourName;
+	}
+}
